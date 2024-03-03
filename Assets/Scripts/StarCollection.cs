c16504a1ad1b2ca558138a3a53c1b8e8c9ce0be1
Telonisse <161740040+Telonisse@users.noreply.github.com>
2024-03-03 19:59:33 +0100

    grr

---
+++ Assets/Scripts/StarCollection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using TMPro;
+
+public class StarCollection : MonoBehaviour
+{
+   
+   private int Star = 0;
+
+    public TextMeshProUGUI starText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Star")
+        {
+            Star++;
+            starText.text = "Stars: " + Star.ToString();
+            Debug.Log(Star);
+            Destroy(other.gameObject);
+        }
+    }
+}
